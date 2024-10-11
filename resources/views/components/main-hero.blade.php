8fc5f39402493155364b81871bf87a4f511a674f
raman47 <ramanjs1313@gmail.com>
2024-10-11 17:58:08 +0530

    Second commit
    Second commit

---
+++ resources/views/components/main-hero.blade.php
@@ -0,0 +1,105 @@
+<header>
+    {{-- <nav x-data="{ isOpen: false }" class="bg-white shadow dark:bg-gray-900">
+        <div class="container px-6 py-4 mx-auto">
+            <div class="lg:flex lg:items-center lg:justify-between">
+                <div class="flex items-center justify-between">
+                    <a href="#" class="mx-auto ">
+                        <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="">
+                    </a>
+
+                    <!-- Mobile menu button -->
+                    <div class="flex lg:hidden">
+                       <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                            <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
+                            </svg>
+
+                            <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0">
+                    <div class="-mx-4 lg:flex lg:items-center">
+                        <a href="#" class="block mx-4 text-gray-700 capitalize dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Web developers</a>
+                        <a href="#" class="block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Web Designers</a>
+                        <a href="#" class="block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">UI/UX Designers</a>
+                        <a href="#" class="block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav> --}}
+
+    <div class="w-full bg-center bg-cover h-[38rem]" style="background-image: url('https://images.unsplash.com/photo-1556761175-b413da4baf72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');">
+        <div class="flex items-center justify-center w-full h-full bg-gray-900/40">
+            <div class="text-center">
+                <div class="border border-gray-300 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
+                    {{-- <div class="flex flex-col md:flex-row">
+                        <div class="">
+                            <select class="border p-2 rounded">
+                                <option>Round-trip</option>
+                                <option>Missouri</option>
+                                <option>texas</option>
+                            </select>
+                        </div>
+                        <div class="pt-6 md:pt-0 md:pl-6">
+                            <select class="border p-2 rounded">
+                                <option>4 Passangers</option>
+                                <option>3 Passangers</option>
+                                <option>2 Passangers</option>
+                            </select>
+                        </div>
+                        <div class="pt-6 md:pt-0 md:pl-6">
+                            <select class="border p-2 rounded">
+                                <option>Economy</option>
+                            </select>
+                        </div>
+                    </div> --}}
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div class="grid grid-cols-2 gap-2 border border-gray-200 p-2 rounded">
+                            <div class="flex border rounded bg-gray-300 items-center p-2 ">
+                                <svg class="fill-current text-gray-800 mr-2 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                    <path class="heroicon-ui"
+                                          d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/>
+                                </svg>
+                                <input type="text" placeholder="Enter text here..."
+                                       class="bg-gray-300 max-w-full focus:outline-none text-gray-700"/>
+                            </div>
+                            <div class="flex border rounded bg-gray-300 items-center p-2 ">
+                                <svg  class="fill-current text-gray-800 mr-2 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                    <path class="heroicon-ui"
+                                          d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM5.68 7.1A7.96 7.96 0 0 0 4.06 11H5a1 1 0 0 1 0 2h-.94a7.95 7.95 0 0 0 1.32 3.5A9.96 9.96 0 0 1 11 14.05V9a1 1 0 0 1 2 0v5.05a9.96 9.96 0 0 1 5.62 2.45 7.95 7.95 0 0 0 1.32-3.5H19a1 1 0 0 1 0-2h.94a7.96 7.96 0 0 0-1.62-3.9l-.66.66a1 1 0 1 1-1.42-1.42l.67-.66A7.96 7.96 0 0 0 13 4.06V5a1 1 0 0 1-2 0v-.94c-1.46.18-2.8.76-3.9 1.62l.66.66a1 1 0 0 1-1.42 1.42l-.66-.67zM6.71 18a7.97 7.97 0 0 0 10.58 0 7.97 7.97 0 0 0-10.58 0z"/>
+                                </svg>
+                                <input type="text" placeholder="Enter text here..."
+                                       class="bg-gray-300 max-w-full focus:outline-none text-gray-700"/>
+                            </div>
+                        </div>
+                        <div class="grid grid-cols-2 gap-2 border border-gray-200 p-2 rounded">
+                            <div class="flex border rounded bg-gray-300 items-center p-2 ">
+                                <svg class="fill-current text-gray-800 mr-2 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                    <path class="heroicon-ui"
+                                          d="M14 5.62l-4 2v10.76l4-2V5.62zm2 0v10.76l4 2V7.62l-4-2zm-8 2l-4-2v10.76l4 2V7.62zm7 10.5L9.45 20.9a1 1 0 0 1-.9 0l-6-3A1 1 0 0 1 2 17V4a1 1 0 0 1 1.45-.9L9 5.89l5.55-2.77a1 1 0 0 1 .9 0l6 3A1 1 0 0 1 22 7v13a1 1 0 0 1-1.45.89L15 18.12z"/>
+                                </svg>
+                                <input type="text" placeholder="Enter text here..."
+                                       class="bg-gray-300 max-w-full focus:outline-none text-gray-700"/>
+                            </div>
+                            <div class="flex border rounded bg-gray-300 items-center p-2 ">
+                                <svg class="fill-current text-gray-800 mr-2 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                    <path class="heroicon-ui"
+                                          d="M13.04 14.69l1.07-2.14a1 1 0 0 1 1.2-.5l6 2A1 1 0 0 1 22 15v5a2 2 0 0 1-2 2h-2A16 16 0 0 1 2 6V4c0-1.1.9-2 2-2h5a1 1 0 0 1 .95.68l2 6a1 1 0 0 1-.5 1.21L9.3 10.96a10.05 10.05 0 0 0 3.73 3.73zM8.28 4H4v2a14 14 0 0 0 14 14h2v-4.28l-4.5-1.5-1.12 2.26a1 1 0 0 1-1.3.46 12.04 12.04 0 0 1-6.02-6.01 1 1 0 0 1 .46-1.3l2.26-1.14L8.28 4zm7.43 5.7a1 1 0 1 1-1.42-1.4L18.6 4H16a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1v5a1 1 0 0 1-2 0V5.41l-4.3 4.3z"/>
+                                </svg>
+                                <input type="text" placeholder="Enter text here..."
+                                       class="bg-gray-300 max-w-full focus:outline-none text-gray-700"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center"><button class="p-2 border w-1/4 rounded-md bg-gray-800 text-white">Search</button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-<x-main-layout></x-main-layout>
+<x-main-layout>
+    <x-slot name="header">
+        <x-main-header></x-main-header>
+    </x-slot>
+    <section>
+        <x-main-hero></x-main-hero>
+    </section>
+    <section>
+        <x-main-section></x-main-section>
+    </section>
+    <x-main-footer></x-main-footer>
+</x-main-layout>
