be3e98c0a859e598334a6e81e7fe581ebc7cf290
raman47 <ramanjs1313@gmail.com>
2024-10-15 18:27:31 +0530

    15-oct-24
    15-oct-24

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories = Category::paginate(12);
+        return  view('admin.categories.index', compact('categories'));
+    }
+    public function create(){
+        return view('admin.categories.create');
+    }
+}
--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -7,17 +7,41 @@
         <span class="ml-3 text-xl">Classified</span>
       </a>
       <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <a class="mr-5 hover:text-gray-900">Homw</a>
+        <a class="mr-5 hover:text-gray-900">Home</a>
         <a class="mr-5 hover:text-gray-900">All Ads</a>
         <a class="mr-5 hover:text-gray-900">Stores</a>
         <a class="mr-5 hover:text-gray-900">Contact</a>
 
         @guest
-            <a class="mr-5 hover:text-gray-900">Login</a>
-            <a class="mr-5 hover:text-gray-900">Register</a>
+            <a href="{{route('login')}}" class="mr-5 hover:text-gray-900">Login</a>
+            <a href="{{route('register')}} class="mr-5 hover:text-gray-900">Register</a>
         @endguest
         @auth
 
+
+        <div @click.away="open = false" class="relative" x-data="{ open: false }">
+            <button @click="open = !open" class="mr-5 hover:text-gray-900">
+                <span class="font-bold">{{ Auth::user()->name }}</span>
+                <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button>
+            <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
+                <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-700">
+
+                    <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="{{route('dashboard')}}">Dashboard</a>
+                    <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="{{ route('profile.show') }}">Profile</a>
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}" x-data>
+                        @csrf
+
+                        <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="{{ route('logout') }}"
+                                @click.prevent="$root.submit();">
+                            Logout
+                        </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+            {{-- <a  class="mr-5 hover:text-gray-900">{{auth()->user()->name}}</a> --}}
         @endauth
       </nav>
       <button class="inline-flex items-center bg-green-500 border-0 py-1 px-3 focus:outline-none hover:bg-green-200 rounded text-base text-gray-50 font-semibold mt-4 md:mt-0">Post New Ad
